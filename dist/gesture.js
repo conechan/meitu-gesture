66d964c4b214daf8cf8922998dd714e09775975f
gxd <gxd@meitu.com>
2017-06-22 11:04:26 +0800

    1.1.1 版本
    修复直接引入包名错误的问题
    增加文档说明

--- dist/gesture.js
+++ dist/gesture.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-	typeof define === 'function' && define.amd ? define('gesture', factory) :
-	(global.gesture = factory());
+	typeof define === 'function' && define.amd ? define('Gesture', factory) :
+	(global.Gesture = factory());
 }(this, (function () { 'use strict';
 
 var _typeof2 = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
